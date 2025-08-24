31d05c4bac797488831c7fc9c2f68f0445a9c8fa
wittydhana <isloordhanashri@gmail.com>
2025-08-24 16:50:00 +0530

    first

---
+++ project2.js
@@ -0,0 +1,57 @@
+const prompt=require("prompt-sync")()
+
+function getNumber(numberString)
+{
+   while(true)
+{
+   number=parseFloat(prompt("Enter the Number "+numberString+":"))
+   if(isNaN(number))
+   {
+      console.log("Invalid input")
+   }
+   else
+   {
+     return number
+   }
+}
+}
+const number1=getNumber('One')
+const number2=getNumber('Two')
+
+
+const operator=prompt("Enter the Sign: ")
+
+let result;
+let valid=true
+switch(operator)
+{
+ case "+":
+    result=number1+number2
+    break;
+ case "-":
+    result=number1-number2
+    break;
+ case "*":
+    result=number1*number2
+    break;
+ case "/":
+   if(number2===0)
+   {
+      valid=false
+      console.log("Zero division error")
+   }
+    result=number1/number2
+    break;
+ case "%":
+    result=number1%number2
+    break;
+ default:
+   console.log("Invalid operator")
+   valid=false;
+   break;
+}
+if(valid) 
+   console.log(number1,operator,number2,"=",result)
+
+
+
